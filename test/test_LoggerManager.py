16f7766755e8d61a723a551124f4265efe3074ab
Atiqul Islam <atiqul.islam@gmail.com>
2022-04-24 23:00:48 -0400

    Fixing unittest code

--- test/test_LoggerManager.py
+++ test/test_LoggerManager.py
@@ -11,12 +11,23 @@ from src.utils import LoggerManager
 
 
 class MyTestCase(unittest.TestCase):
-    def setUp(self) -> None:
+    @classmethod
+    def setUpClass(cls) -> None:
         env = os.environ
         filename = uuid.uuid4().hex + ".log"
-        self.logfile = path.join( (env['TEMP'] if sys.platform == "win32" else "/tmp" ), filename)
-        self.logger = LoggerManager.getlogger(name="test", logfile=self.logfile)
-        self.logger2 = LoggerManager.getlogger(name="junk")
+        cls.logfile = path.join( (env['TEMP'] if sys.platform == "win32" else "/tmp" ), filename)
+        cls.logger = LoggerManager.getlogger(name="test", logfile=cls.logfile)
+        cls.logger2 = LoggerManager.getlogger(name="junk")
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        pass
+
+    def setUp(self) -> None:
+        pass
+
+    def tearDown(self) -> None:
+        pass
 
     def test_logfile_creation(self):
         self.assertTrue(path.exists(self.logfile))
